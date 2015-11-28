6dd40b1d7c6757f2136da91b231200faf6a412c3
Thomas Leonard <talex5@gmail.com>
2015-11-28 17:25:48 +0000

    Exit when requested by Xen's control/shutdown
    This allows the VM to be stopped using the Qubes GUI.

--- unikernel.ml
+++ unikernel.ml
@@ -24,11 +24,30 @@ module Main (C: V1_LWT.CONSOLE) = struct
     | "echo" -> echo ~user flow
     | cmd -> error "Unknown command %S" cmd
 
+  let wait_for_shutdown () =
+    let module Xs = OS.Xs in
+    Xs.make () >>= fun xs ->
+    Xs.immediate xs (fun h -> Xs.read h "domid") >>= fun domid ->
+    let domid = int_of_string domid in
+    Xs.immediate xs (fun h -> Xs.getdomainpath h domid) >>= fun domainpath ->
+    Xs.wait xs (fun xsh ->
+      Xs.read xsh (domainpath ^ "/control/shutdown") >>= function
+      | "poweroff" -> return `Poweroff
+      | "" -> fail Xs_protocol.Eagain
+      | state ->
+          Log.info "Unknown power state %S" state >>= fun () ->
+          fail Xs_protocol.Eagain
+    )
+
   let start c =
     Log.reporter := (fun lvl msg -> C.log_s c (lvl ^ ": " ^ msg));
     Qrexec.connect ~domid:0 () >>= fun qrexec ->
     let agent_listener = Qrexec.listen qrexec (handler qrexec) in
     Log.info "Starting gui-agent; waiting for client..." >>= fun () ->
     Vchan_xen.server ~domid:0 ~port:gui_agent_port () >>= fun _gui ->
+    Lwt.async (fun () ->
+      wait_for_shutdown () >>= fun `Poweroff ->
+      Qrexec.disconnect qrexec
+    );
     agent_listener
 end
