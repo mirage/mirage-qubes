97bafc4117b45ce4a5ec5c1de8392c2f120443fa
Mindy Preston <mindy.preston@docker.com>
2016-11-29 19:10:04 -0600

    use static /32 for netmask.

--- lib/ipv4/qubesdb_ipv4.ml
+++ lib/ipv4/qubesdb_ipv4.ml
@@ -7,11 +7,10 @@ module Make(D: Qubes.S.DB) (Ethernet : V1_LWT.ETHIF) (Arp : V1_LWT.ARP) = struct
       | Some s -> f s
     in
     let ip = D.read db "/qubes-ip" >>=? Ipaddr.V4.of_string in
-    let netmask = D.read db "/qubes-netmask" >>=? Ipaddr.V4.of_string in
     let gateway = D.read db "/qubes-gateway" >>=? Ipaddr.V4.of_string in
-    match (ip, netmask) with
-    | Some ip , Some netmask ->
-      let network = Ipaddr.V4.Prefix.of_netmask netmask ip in
+    match ip with
+    | Some ip ->
+      let network = Ipaddr.V4.Prefix.make 32 ip in
       IP.connect ~ip ~network ~gateway ethif arp
     | _ -> Lwt.fail_with "couldn't get ip configuration from qubesdb"
 end
