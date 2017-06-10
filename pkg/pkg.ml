66f518967cad296c9e78a541e86709ea19ae4939
David Scott <dave@recoil.org>
2017-06-10 15:56:32 +0100

    Switch to jbuilder and topkg
    - split lib/qubes.mli into one interface per module
    - split ipv4 configurator into mirage-qubes-ipv4
    
    Signed-off-by: David Scott <dave@recoil.org>

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,16 +1,3 @@
 #!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let ipv4 = Conf.(key
-                 ~absent:false
-                 ~doc:"Build automatic IPv4 network configurators"
-                 "with-ipv4"
-                 Conf.bool
-)
-let () =
-  Pkg.describe "mirage-qubes" @@ fun c ->
-  Ok [ Pkg.mllib ~api:["Qubes"] "lib/qubes.mllib";
-       Pkg.mllib ~cond:(Conf.value c ipv4) "lib/ipv4/qubesdb_ipv4.mllib";
-  ]
+#require "topkg-jbuilder.auto"
