8bb90adb40bc608e6f4e07865fc95dd20c4a4206
Reynir Björnsson <reynir@reynir.dk>
2019-09-30 10:35:36 +0200

    lib/rExec.mli: Break long lines

--- lib/rExec.mli
+++ lib/rExec.mli
@@ -22,12 +22,15 @@ module Client_flow : sig
 end
 
 type handler = user:string -> string -> Flow.t -> int Lwt.t
-(** A handler gets a command-line and a two-way connection to the requesting client each time
-    the remote client sends a MSG_EXEC_CMDLINE request. The "exit code" from the handler
-    is returned to the client. *)
+(** A handler gets a command-line and a two-way connection to the requesting
+   client each time the remote client sends a MSG_EXEC_CMDLINE request. The
+   "exit code" from the handler is returned to the client. *)
 
 type client = [`Ok of Client_flow.t | `Closed | `Permission_denied | `Error of string]  -> unit Lwt.t
-(** [client] is a callback for unikernel-initiated qrexec calls. The callback is called exactly once, and is given either a flow if successful, [`Closed] if the control channel has been closed, [`Permission_denied] if dom0 says so, or an error. *)
+(** [client] is a callback for unikernel-initiated qrexec calls. The callback
+   is called exactly once, and is given either a flow if successful, [`Closed]
+   if the control channel has been closed, [`Permission_denied] if dom0 says
+   so, or an error. *)
 
 val connect : domid:int -> unit -> t Lwt.t
 (** [connect ~domid ()] is a qrexec agent to which a client in [domid] has connected.
@@ -40,7 +43,9 @@ val listen :  t -> handler -> unit Lwt.t
     the client disconnects. *)
 
 val qrexec : t -> vm:string -> service:string -> client -> [`Ok | `Closed] Lwt.t
-(** [qrexec t ~vm ~service ~client] initiates a qrexec call to [vm]'s service [service], and calls [client] with the result. If the control channel to dom0 is closed the result is [`Closed]. Otherwise [`Ok] is returned. *)
+(** [qrexec t ~vm ~service ~client] initiates a qrexec call to [vm]'s service
+   [service], and calls [client] with the result. If the control channel to
+   dom0 is closed the result is [`Closed]. Otherwise [`Ok] is returned. *)
 
 val disconnect : t -> unit Lwt.t
 (** Close the underlying vchan. This will cause any listening thread to finish. *)
