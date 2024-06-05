c0d18a8eddf8657158cacabb82ee3ec616261687
Pierre Alain <pierre.alain@tuta.io>
2024-06-05 16:37:29 +0200

    update prefix type for mirage-tcpip 8.1.0

--- lib/ipv4/qubesdb_ipv4.mli
+++ lib/ipv4/qubesdb_ipv4.mli
@@ -5,7 +5,7 @@ module Make
     (Ethernet : Ethernet.S)
     (Arp : Arp.S) : sig
 
-  include Tcpip.Ip.S with type ipaddr = Ipaddr.V4.t
+  include Tcpip.Ip.S with type ipaddr = Ipaddr.V4.t and type prefix = Ipaddr.V4.Prefix.t
   val connect : D.t -> Ethernet.t -> Arp.t -> t Lwt.t
   (** [connect db ethernet arp] attempts to use the provided [db]
    *  to look up the correct IPV4 information, and construct
