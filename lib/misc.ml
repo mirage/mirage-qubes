5a169af84b90b28f3800d5e54b4f3e3943da8e78
Pierre Alain <pierre.alain@tuta.io>
2024-07-24 18:00:28 +0200

    apply review suggestion, add optional argument to specify the limit before triggering a collection

--- lib/misc.ml
+++ lib/misc.ml
@@ -1,15 +1,15 @@
 
-let check_memory () =
+let check_memory ?(fraction=0.4) () =
   let fraction_free stats =
     let { Xen_os.Memory.free_words; heap_words; _ } = stats in
     float free_words /. float heap_words
   in
   let stats = Xen_os.Memory.stat () in
-  if fraction_free stats > 0.4 then `Ok
+  if fraction_free stats > fraction then `Ok
   else (
     Gc.full_major ();
     let stats = Xen_os.Memory.quick_stat () in
-    if fraction_free stats < 0.4 then `Memory_critical
+    if fraction_free stats < fraction then `Memory_critical
     else `Ok
   )
 
